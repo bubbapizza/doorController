0953aaedb11d6105ac6d5b3d50fe4745782d39e9
Shawn Wilson <shawn@ch2a.ca>
2013-09-13 11:44:15 -0400

    Starting to rewrite for parallax RFID reader

--- door-controller.ino
+++ door-controller.ino
@@ -12,25 +12,51 @@
  *
  *************/
 
-#include "Wiegand.h"
+#include <SoftwareSerial.h>
 
-/* CONSTANTS */
-#define VERSION 0.1
 
-/* This is the pin that triggers the door lock on and off. */
-#define DOOR_LOCK_PIN 9
+/**** CONSTANTS ****/
+
+#define VERSION 0.2
+
+/* Set up pins for lights, buzzer(s), door lock, and RFID enable. */
+#define GREEN_LED_PIN 4
+#define RED_LED_PIN 5
+#define BELL_PIN 6
+#define DOOR_LOCK_PIN 7
+#define RFID_ENABLE_PIN 2
+
+/* The Parallax RFID reader is a read-only device, but we still have
+   to set up two pins for the software serial driver.  The SOUT pin
+   of the Parallax reader gets connected to the Rx pin and the 
+   Tx pin is unused. */
+#define RFID_RX_PIN 8
+#define RFID_TX_PIN 9
 
 /* Set up commands triggered by incoming characters over serial. */
 #define CMD_LOCK 'l'
 #define CMD_UNLOCK 'u'
+#define CMD_TOGGLE_GREEN 'g'
+#define CMD_TOGGLE_RED 'r'
+#define CMD_BELL 'b'
+#define CMD_RFID_ENABLE 'e'
+#define CMD_RFID_DISABLE 'd'
 
 /* Set up message strings to be sent back over serial. */
 #define MSG_LOCK "DOOR LOCKED"
 #define MSG_UNLOCK "DOOR UNLOCKED"
+#define MSG_GREEN_OFF "GREEN LED OFF"
+#define MSG_GREEN_ON "GREEN LED ON"
+#define MSG_RED_OFF "RED LED OFF"
+#define MSG_RED_ON "RED LED ON"
+#define MSG_BELL "BELL RING"
+#define MSG_RFID_ENABLE "RFID READER ENABLED"
+#define MSG_RFID_DISABLED "RFID READER DISABLED"
 
 
-/* VARIABLES */
-WIEGAND wg;
+/**** GLOBAL VARIABLES *****/
+
+SoftwareSerial rfidReader(RFID_RX_PIN, RFID_TX_PIN);
 
 
 
@@ -47,18 +73,42 @@ void unlockDoor() {
 
 
 /**********
+ *  RFID enable/disable routines.
+ **********/
+void disableRFID() {
+   digitalWrite(RFID_ENABLE_PIN, HIGH);
+} /* endfunction */
+
+void enableRFID() {
+   digitalWrite(RFID_ENABLE_PIN, LOW);
+} /* endfunction */
+
+
+/**********
  *  Startup routine
  **********/
 void setup() {
+
+   /* Initialize communications. */
    Serial.begin(9600);
+   rfidReader.begin(2400);
+
+   /* Initialize pins. */
+   pinMode(GREEN_LED_PIN, OUTPUT);  
+   pinMode(RED_LED_PIN, OUTPUT);  
+   pinMode(BELL_PIN, OUTPUT);  
    pinMode(DOOR_LOCK_PIN, OUTPUT);  
+   pinMode(RFID_ENABLE_PIN, OUTPUT);  
 
-   /* Start listening for RFID tags and lock the door. */
-   wg.begin();
+   /* Start listening for RFID tags, lock the door and turn on 
+      the red LED. */
    lockDoor();
+   enableRFID();
+   digitalWrite(RED_LED_PIN, HIGH);
 
-   Serial.println("Door controller initialized.");
+   Serial.println("/nDoor controller initialized.");
 } /* endfunction */
+
 
 
 /**********
@@ -70,7 +120,7 @@ void loop() {
 
    /* See if we got any info that came in over the Wiegand protocol. 
       i.e.  See if a card was swiped. */
-   if (wg.available()) {
+   if (rfidReader.available()) {
 
       /* When we get a card, print the card info in hex followed
          by the card type (8, 26 or 34). */
