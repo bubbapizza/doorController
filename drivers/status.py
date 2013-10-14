37d8e1d35bd95a430a030b0490bd3fa8e166ebfa
Shawn Wilson <shawn@ch2a.ca>
2013-10-14 17:13:13 -0400

    Did some directory re-arranging

---
+++ drivers/status.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+#
+# This file defines all the door controller parts and their statuses
+
+DOOR = "door"
+LOCKED = "locked"
+UNLOCKED = "unlocked"
+
+GREEN = "greenLED"
+RED = "redLED"
+ON = True
+OFF = False
+
+RFID = "rfid"
+ENABLED = "enabled"
+DISABLED = "disabled"
+
+
