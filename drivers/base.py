bfe410bf0e9b461884ff20c9f6a3da3ce23632b3
Shawn Wilson <shawn@ch2a.ca>
2013-11-03 13:16:46 -0500

    First try at base driver class

---
+++ drivers/base.py
@@ -0,0 +1,230 @@
+#!/usr/bin/python
+
+#        Copyright (C) 2013 Shawn Wilson
+#        shawn@ch2a.ca
+#        
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#
+# This is the base class for creating a door controller device driver.
+# It can read RFID cards, unlock/locking a door and controlling some 
+# lights and a buzzer for indicating swipe card status.
+#
+
+
+from status import *
+
+
+####### EXCEPTIONS #######
+class Error(Exception):
+   """Base class for exceptions in this module."""
+   pass
+
+class StatusError(Error):
+   """Exception raised when the controller status is bad/incomplete.
+
+   Attributes:
+      status -- the bad/incomplete status dictionary"""
+
+   def __init__(self, status):
+      self.status = status
+
+   
+
+####### CONTROLLER OBJECT #######
+
+class controller:
+   """This is a device driver for arduino-based door controller hardware.
+   The arduino has to be running the doorController.ino sketch that comes
+   with this package."""
+
+
+   def __init__(self, cardFile=None):
+      """Initializing the controller turns on the red LED on
+      the swipe card unit, turns the green LED off, locks the door,
+      and starts reading RFID cards."""
+
+      self.status = {
+         GREEN : OFF, 
+         RED : OFF,
+         DOOR : LOCKED
+         RFID : ENABLED
+      }
+      print("Controller initialized!!")
+
+
+   ##### BASE METHODS #####
+
+   def readRFID(self)
+      """This routine returns the RFID card value if one happens to 
+      be swiped.  Normally, this routine would block on the
+      hardware-specific _readRFID method."""
+
+      card = None
+      if self.status[RFID]
+         card = self._readRFID()
+
+      return card
+
+   
+   def statusRFID(self):
+      """Returns whether or not RFID reading is enabled.  It returns the
+      values ENABLED or DISABLED."""
+
+      status = self._readStatus()
+      return status[RFID]
+      
+   def statusDoor(self):
+      """Returns whether the door is LOCKED or UNLOCKED."""
+
+      status = self._readStatus()
+      return status[DOOR]
+
+
+   def lockDoor(self):
+      """Lock the door."""
+      self._sendCMD(CMD_DOOR_LOCK)
+
+   def unlockDoor(self):
+      """Unlock the door."""
+      self._sendCMD(CMD_DOOR_UNLOCK)
+
+
+   def greenON(self):
+      """Turn the green LED on."""
+      self._sendCMD(CMD_GREEN_ON)
+
+   def greenOFF(self):
+      """Turn the green LED off."""
+      self._sendCMD(CMD_GREEN_OFF)
+
+
+   def redON(self):
+      """Turn the red LED on."""
+      self._sendCMD(CMD_RED_ON)
+
+   def redOFF(self):
+      """Turn the red LED off."""
+      self._sendCMD(CMD_RED_OFF)
+
+
+   def ringBell(self):
+      """Ring a buzzer."""
+      self._sendCMD(CMD_BELL)
+
+   
+   def rfidON(self):
+      """Enable reading of RFID cards."""
+      self._sendCMD(CMD_RFID_ENABLE)
+
+   def rfidOFF(self):
+      """Disable reading of RFID cards."""
+      self._sendCMD(CMD_RFID_DISABLE)
+
+
+
+   ##### OVERRIDE THESE HARDWARE SPECIFIC METHODS #####
+
+   def _readRFID(self):
+      """Return the card number of the most recently read RFID card."""
+
+      print("Reading RFID swipe card...")
+
+      return ""
+
+
+   def _sendCMD(self, cmd):
+      """Internally used to for controlling pins and returning 
+      status codes."""
+
+      print("Sending command:", cmd)
+
+      returnVal = None
+
+      #
+      # Return the status of all pollable parts.
+      #
+      if cmd == CMD_STATUS:
+         returnVal = []
+         if GPIO.input(PIN_RED) == GPIO.HIGH:
+            returnVal.append(MSG_RED_ON)
+         elif GPIO.input(PIN_RED) == GPIO.LOW:
+            returnVal.append(MSG_RED_OFF)
+         
+         if GPIO.input(PIN_GREEN) == GPIO.HIGH:
+            returnVal.append(MSG_GREEN_ON)
+         elif GPIO.input(PIN_GREEN) == GPIO.LOW:
+            returnVal.append(MSG_GREEN_OFF)
+
+         if GPIO.input(PIN_DOOR) == GPIO.HIGH:
+            returnVal.append(MSG_LOCK)
+         elif GPIO.input(PIN_DOOR) == GPIO.LOW:
+            returnVal.append(MSG_UNLOCK)
+         
+         if GPIO.input(PIN_RFID) == GPIO.HIGH:
+            returnVal.append(MSG_RFID_ENABLED)
+         elif GPIO.input(PIN_RFID) == GPIO.LOW:
+            returnVal.append(MSG_RFID_DISABLED)
+
+         if len(returnVal) != NUM_STATUS_POLLABLE:
+            raise StatusError
+
+      
+      #
+      # Change the red LED status.
+      #
+      elif cmd == CMD_RED_ON:
+         GPIO.output(PIN_RED, GPIO.HIGH)
+      elif cmd == CMD_RED_OFF:
+         GPIO.output(PIN_RED, GPIO.LOW)
+
+      #
+      # Change the Green LED status.
+      #
+      elif cmd == CMD_GREEN_ON:
+         GPIO.output(PIN_GREEN, GPIO.HIGH)
+      elif cmd == CMD_GREEN_OFF:
+         GPIO.output(PIN_GREEN, GPIO.LOW)
+
+      #
+      # Change the door lock status.
+      #
+      elif cmd == CMD_DOOR_LOCK:
+         GPIO.output(PIN_DOOR, GPIO.HIGH)
+      elif cmd == CMD_DOOR_UNLOCK:
+         GPIO.output(PIN_DOOR, GPIO.LOW)
+
+      #
+      # Change the door lock status.
+      #
+      elif cmd == CMD_RFID_ENABLE:
+         GPIO.output(PIN_RFID, GPIO.HIGH)
+      elif cmd == CMD_RFID_DISABLE:
+         GPIO.output(PIN_RFID, GPIO.LOW)
+
+      #
+      # Ring the piezo buzzer.
+      # 
+      elif cmd == BELL:
+         print MSG_BELL
+
+
+      return returnVal
+
+
+   def _readStatus(self):
+      """Get the current status of all the door controller devices."""
+   
+      return self.status
+
+
